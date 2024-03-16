43626741e85ae67493f69fdb27851141c896d2ac
marianorapa <marianorapaport@gmail.com>
2024-03-15 23:43:13 -0300

    disable CSRF

--- src/main/java/com/auth0/example/security/SecurityConfig.java
+++ src/main/java/com/auth0/example/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,7 +26,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(authz -> authz
             .requestMatchers("/api/private", "/api/private-scoped").authenticated()
             .anyRequest().permitAll())
-        .cors(Customizer.withDefaults())
+        .cors(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable)
         .oauth2ResourceServer(oauth2 -> oauth2
             .jwt(jwt -> jwt.jwtAuthenticationConverter(makePermissionsConverter()))
             .authenticationEntryPoint(authenticationErrorHandler))
