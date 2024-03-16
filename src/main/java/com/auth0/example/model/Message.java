e8a888f5153ee0af4b253bda73f5a32e6edfb755
marianorapa <marianorapaport@gmail.com>
2024-03-15 23:43:00 -0300

    create Message table and entity

--- src/main/java/com/auth0/example/model/Message.java
+++ src/main/java/com/auth0/example/model/Message.java
@@ -1,6 +1,35 @@
 package com.auth0.example.model;
 
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
 /**
  * Simple domain object for our API to return a message.
  */
-public record Message(String message) {}
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Message {
+
+    @Id
+    private UUID id;
+
+    private String message;
+
+    @PrePersist
+    void prePersist() {
+        this.id = UUID.randomUUID();
+    }
+
+    public Message(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/resources/db/migration/V1__message.sql
@@ -0,0 +1,5 @@
+ create table public.message (
+     id uuid not null,
+     message text not null,
+     primary key (id));
+
